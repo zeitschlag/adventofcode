6b28d3b4aee6037de676a5206de7510e1348a701
Nathan Mattes <hallo@bullenscheisse.de>
2018-12-17 08:43:57 +0100

    Add solution for second puzzle [05]

--- 2018/05/fifth_day.py
+++ 2018/05/fifth_day.py
@@ -18,8 +18,6 @@ def first_puzzle(input_file_path):
     pattern = "(" + pattern + ")"
 
     while unpolarity_found:
-        # search for lowercaseUPPERCASE
-
         result = re.search(pattern=pattern, string=content_of_file)
 
         if result:
@@ -30,6 +28,53 @@ def first_puzzle(input_file_path):
 
     print("Length of the polymer is %s" % (len(content_of_file)))
 
+def second_puzzle(input_file_path):
+    content_of_file = open(input_file_path, "r").read()
+    shortest_polymer = len(content_of_file)
+    reaction_regex_list = list()
+
+    for i in string.ascii_lowercase:
+        for j in string.ascii_uppercase:
+            if i.lower() == j.lower():
+                reaction_regex_list.append("%s%s" % (i, j))
+                reaction_regex_list.append("%s%s" % (j, i))
+
+    reaction_pattern = "|".join(reaction_regex_list)
+    reaction_pattern = "(" + reaction_pattern + ")"
+
+    for lowercase in string.ascii_lowercase:
+        UPPERCASE = lowercase.upper()
+
+        content_of_file = open(input_file_path, "r").read()
+        pattern = "(%s|%s)" % (lowercase, UPPERCASE)
+
+        # remove all letters of one kind
+        all_letters_replaced = True
+        while all_letters_replaced:
+            result = re.search(pattern=pattern, string=content_of_file)
+            if result:
+                for group in result.groups():
+                    content_of_file = content_of_file.replace(group, "")
+            else:
+                all_letters_replaced = False
+
+        # do the usual reaction-thing
+        unpolarity_found = True
+        while unpolarity_found:
+            result = re.search(pattern=reaction_pattern, string=content_of_file)
+
+            if result:
+                for group in result.groups():
+                    content_of_file = content_of_file.replace(group, "")
+            else:
+                unpolarity_found = False
+
+        if len(content_of_file) < shortest_polymer:
+            shortest_polymer = len(content_of_file)
+
+    print("Length of shortest polymer: %s" % (shortest_polymer))
+
 
 if __name__ == "__main__":
     first_puzzle("input.txt")
+    second_puzzle("input.txt")
