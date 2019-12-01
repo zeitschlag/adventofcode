694416aaa7dbde9b7f51918fff79135874e338fa
Nathan Mattes <hallo@bullenscheisse.de>
2019-12-01 18:57:13 +0100

    Add solution for second puzzle of first day

--- 2019/01/first_day.rb
+++ 2019/01/first_day.rb
@@ -3,11 +3,23 @@ def calculate_fuel(module_mass)
   return module_fuel
 end
 
+def calculate_total_fuel(mass)
+  fuel = calculate_fuel(mass)
+  additional_fuel = calculate_fuel(fuel)
+
+  while additional_fuel >= 0 do
+    fuel += additional_fuel
+    additional_fuel = calculate_fuel(additional_fuel)
+  end
+
+  return fuel
+end
+
 required_fuel = 0
 
 File.open("input.txt", "r") do |file|
   file.each_line do |module_mass|
-    required_fuel += calculate_fuel(module_mass)
+    required_fuel += calculate_total_fuel(module_mass)
   end
 end
 
