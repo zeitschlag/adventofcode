0c1679ace7f73c73af7591e463bec2c7f97904c3
Nathan Mattes <hallo@bullenscheisse.de>
2019-12-03 16:29:17 +0100

    [WIP] start coordinate calculation

--- 2019/03/third_day.rb
+++ 2019/03/third_day.rb
@@ -13,7 +13,7 @@ class Coordinate
 
 end
 
-class Path
+class Wire_Path
   def initialize(central_port, instructions)
     @central_port = central_port
 
@@ -21,14 +21,24 @@ class Path
   end
 
   def get_coordinates_from_instructions(instructions)
+    current_point = @central_port
+    instruction_list = instructions.split(",")
+  
+    instruction_list.each do |instruction|
+      direction = instruction[0]
+      
+    end
     return [Coordinate.new(1, 1)]
   end
 end
 
 central_port = Coordinate.new(x=0, y=0)
 
+wire_instructions = IO.read("input.txt").split("\n")
+
+
 # first idea:
-# central port: 0, 0
+# central port: 0, 0, although it doesn't matter
 # read wire-paths, each path is in one line
 # calculate all points for all paths
 # crossing points are points, that are in both paths
