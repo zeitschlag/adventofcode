15863b1f331a5f120b7392f68006f30a38435a02
Nathan Mattes <hallo@bullenscheisse.de>
2019-12-03 10:44:59 +0100

    Draft idea for first task of third day

---
+++ 2019/03/third_day.rb
@@ -0,0 +1,7 @@
+# first idea:
+# central port: 0, 0
+# read wire-paths, each path is in one line
+# calculate all points for all paths
+# crossing points are points, that are in both paths
+# calculate the manhattan-distance for all crossing points to the central port
+# loweset manhattan distance is the answer
