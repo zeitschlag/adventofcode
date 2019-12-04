6c4a62177dbc9ef6ada2949fb952d825ad1bdfc0
Nathan Mattes <hallo@bullenscheisse.de>
2019-12-04 18:07:15 +0100

    [WIP] think about second puzzle

--- 2019/04/fourth_day.rb
+++ 2019/04/fourth_day.rb
@@ -1,4 +1,3 @@
-
 def is_number_potential_password?(number)
   # has six digits
   has_six_digits = number.to_s.length == 6
@@ -18,8 +17,13 @@ def is_number_potential_password?(number)
   return (has_six_digits && never_decrease && two_adjacent_digits)
 end
 
-def first_puzzle
+def has_exactly_two_digits(number)
+  # 112233 is valid
+  # 123444 is not valid
+  # 111122 is invalid (22)
+end
 
+def first_puzzle
   potential_passwords = []
 
   (353096..843212).each { |current_number|
@@ -28,7 +32,22 @@ def first_puzzle
 	end
   }
   
-  puts "Potential Passwords: #{potential_passwords.length}"
+  puts "1. Potential Passwords: #{potential_passwords.length}"
+end
+
+def second_puzzle
+
+  potential_passwords = []
+
+  (353096..843212).each { |current_number|
+  	if is_number_potential_password?(current_number) && has_exactly_two_digits(current_number)
+	  potential_passwords.append(current_number)
+	end
+  }
+  
+  
+  puts "2. Potential Passwords: #{potential_passwords.length}"
+
 end
 
 first_puzzle
