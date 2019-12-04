9c3b621856c91d8147c13aca4e406dabb727ff62
Nathan Mattes <hallo@bullenscheisse.de>
2019-12-04 16:43:27 +0100

    Clean up things

--- 2019/04/fourth_day.rb
+++ 2019/04/fourth_day.rb
@@ -1,20 +1,30 @@
-potential_passwords = []
 
-(353096..843212).each { |current_number|
+def is_number_potential_password?(number)
   # has six digits
-  has_six_digits = current_number.to_s.length == 6
+  has_six_digits = number.to_s.length == 6
   # two adjacent digits are the same
   two_adjacent_digits = false
-  number_string = current_number.to_s
+  number_string = number.to_s
   if number_string[0] == number_string[1] || number_string[1] == number_string[2] || number_string[2] == number_string[3] || number_string[3] == number_string[4] || number_string[4] == number_string[5]
     two_adjacent_digits = true
   end
   # digits never decrease. Sort and compare if strings are the same
-  never_decrease = current_number.to_s.chars.sort == current_number.to_s.chars
+  never_decrease = number.to_s.chars.sort == number.to_s.chars
 
-  if has_six_digits && never_decrease && two_adjacent_digits
-    potential_passwords.append(current_number)
-  end
-}
+  return (has_six_digits && never_decrease && two_adjacent_digits)
+end
 
-puts "Potential Passwords: #{potential_passwords.length}"
+def first_puzzle
+
+  potential_passwords = []
+
+  (353096..843212).each { |current_number|
+  	if is_number_potential_password?(current_number)
+	  potential_passwords.append(current_number)
+	end
+  }
+  
+  puts "Potential Passwords: #{potential_passwords.length}"
+end
+
+first_puzzle
