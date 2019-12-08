b6f414df28862191d843d6543bf8dac88176133c
Nathan Mattes <hallo@bullenscheisse.de>
2019-12-08 13:13:35 +0100

    Add tests for 7.1

--- 2019/07/7.rb
+++ 2019/07/7.rb
@@ -1,3 +1,5 @@
+require 'minitest/autorun'
+
 POSITIONMODE = "0"
 IMMEDIATEMODE = "1"
 
@@ -165,3 +167,23 @@ def first_puzzle
 end
 
 first_puzzle
+
+class ThrustTests < MiniTest::Test
+  def test_43210
+    intcode = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0".split(",")
+    thrust = calculate_thrust(intcode, [0,1,2,3,4])
+    assert_equal 43210, thrust
+  end
+  
+  def test_54321
+    intcode = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0".split(",")
+    thrust = calculate_thrust(intcode, [0,1,2,3,4])
+    assert_equal 54321, thrust
+  end
+  
+  def test_65210
+    intcode = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0".split(",")
+    thrust = calculate_thrust(intcode, [0,1,2,3,4])
+    assert_equal 65210, thrust
+  end
+end
