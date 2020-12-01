8ed8ed814f6defcfe8d82612cebbf79d898a464e
Nathan Mattes <hallo@bullenscheisse.de>
2020-12-01 18:41:51 +0100

    Solve second puzzle in a quick and dirty manner

--- 2020/01/01.py
+++ 2020/01/01.py
@@ -7,9 +7,10 @@ expense_list = open("input.txt", "r").readlines()
 
 for a in expense_list:
     for b in expense_list:
-        sum = int(a)+int(b)
+        for c in expense_list:
+            sum = int(a)+int(b)+int(c)
 
-        if sum==2020:
-            result = int(a)*int(b)
-            print("{a}*{b}={result}".format(a=a.strip(), b=b.strip(), result=str(result)))
-            exit()
+            if sum==2020:
+                result = int(a)*int(b)*int(c)
+                print("{a}*{b}*{c}={result}".format(a=a.strip(), b=b.strip(), c=c.strip(), result=str(result)))
+                exit()
