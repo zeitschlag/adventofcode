3ee3a32879d0d1f2466822d1ab4dc482ba827536
Nathan Mattes <hallo@bullenscheisse.de>
2020-12-04 17:17:41 +0100

    Calculate hit trees for multile slopes
    That was way easier than expected

--- 2020/03/03.py
+++ 2020/03/03.py
@@ -1,30 +1,45 @@
 def read_file(name: str):
     return open(name, "r").readlines()
 
+
 def is_tree(character: str):
     return character == "#"
 
 
 if __name__ == '__main__':
 
-    hill_map = read_file("input.txt")
-    x = 0
-    y = 0
-    delta_x = 3
-    delta_y = 1
-    hit_count = 0
+    from collections import namedtuple
 
-    while y < len(hill_map)-1:
-        y += delta_y
-        x += delta_x
+    Slope = namedtuple("Slope", "x, y")
+    slopes = [
+        Slope(x=1, y=1),
+        Slope(x=3, y=1),
+        Slope(x=5, y=1),
+        Slope(x=7, y=1),
+        Slope(x=1, y=2),
+    ]
 
-        current_line = hill_map[y].strip()
-        fixed_x = x % len(current_line) # linked list for the poor using modulo
+    answer = 1
 
-        character = current_line[fixed_x]
+    for slope in slopes:
+        hit_count = 0
 
-        if is_tree(character=character):
-            print("Ouch! {0}, {1})".format(y, fixed_x))
-            hit_count += 1
+        hill_map = read_file("input.txt")
+        x = 0
+        y = 0
 
-    print("We hit {0} trees".format(hit_count))
+        while y < len(hill_map)-1:
+            y += slope.y
+            x += slope.x
+
+            current_line = hill_map[y].strip()
+            fixed_x = x % len(current_line) # linked list for the poor using modulo
+            character = current_line[fixed_x]
+
+            if is_tree(character=character):
+                hit_count += 1
+
+        print("We hit {0} trees".format(hit_count))
+        answer *= hit_count
+
+    print("Answer is {0}".format(answer))
