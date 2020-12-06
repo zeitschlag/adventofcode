b7e04eb8a39ff957a4adb8c5ed21ccce5daa0935
Nathan Mattes <hallo@bullenscheisse.de>
2020-12-06 10:02:41 +0100

    day six, puzzle one

---
+++ 2020/06/day_six.py
@@ -0,0 +1,29 @@
+def read_group_answers(filename: str):
+    raw_input = open(filename, "r").read()
+    return raw_input.split("\n\n")
+
+
+def count_all_answers_in_group(group_answer: str):
+    # get rid of newlines
+    raw_string = group_answer.replace("\n", "")
+
+    # create set and put every char of the string in it
+    unique_answers = set(list(raw_string))
+
+    return len(unique_answers)
+
+
+def first_puzzle():
+    group_answers = read_group_answers(filename="answers.txt")
+    all_answers_per_group = list()
+
+    for group_answer in group_answers:
+        answer_in_group = count_all_answers_in_group(group_answer=group_answer)
+        all_answers_per_group.append(answer_in_group)
+
+    all_answers = sum(all_answers_per_group)
+    print("Sum of answers: {0}".format(all_answers))
+
+
+if __name__ == "__main__":
+    first_puzzle()
