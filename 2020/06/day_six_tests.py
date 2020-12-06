35f030e154660e6b2d35dc518ad56204611995a3
Nathan Mattes <hallo@bullenscheisse.de>
2020-12-06 11:10:57 +0100

    Remove some whitespace

--- 2020/06/day_six_tests.py
+++ 2020/06/day_six_tests.py
@@ -9,13 +9,11 @@ class DaySixPuzzleOneTests(unittest.TestCase):
         result = count_all_answers_in_group(group_answer=group_answer)
         self.assertEqual(expected, result)
 
-
     def test_count_all_answers_in_group_abc_new_line(self):
         group_answer = "a\nb\nc"
         expected = 3
         result = count_all_answers_in_group(group_answer=group_answer)
         self.assertEqual(expected, result)
-
 
     def test_count_all_answers_in_group_acab(self):
         group_answer = "ab\nac"
@@ -23,13 +21,11 @@ class DaySixPuzzleOneTests(unittest.TestCase):
         result = count_all_answers_in_group(group_answer=group_answer)
         self.assertEqual(expected, result)
 
-
     def test_count_all_answers_in_group_aaaa(self):
         group_answer = "a\na\na\na"
         expected = 1
         result = count_all_answers_in_group(group_answer=group_answer)
         self.assertEqual(expected, result)
-
 
     def test_count_all_answers_in_group_b(self):
         group_answer = "b"
