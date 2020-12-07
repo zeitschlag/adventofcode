3281f81f5ce9c1d4fedcee452526a3d76f079ab1
Nathan Mattes <hallo@bullenscheisse.de>
2020-12-07 09:13:13 +0100

    Day 7, puzzle 2
    this was a pita. the unit tests made me guess the code. this must be TDD, not?!

--- 2020/07/day_seven.py
+++ 2020/07/day_seven.py
@@ -4,7 +4,7 @@ def read_rules(filename: str):
 
 
 def parse_rules(list_of_rules: list):
-    # rule = {"color": ["color", color"]}
+    # rule = {"color": [{"color": "color", "amount"}, color"]}
     rules = dict()
 
     for raw_rule in list_of_rules:
@@ -19,9 +19,10 @@ def parse_rules(list_of_rules: list):
             bag_color_list = bag_colors.replace(".", "").split(",")
             values = list()
             for bag_color in bag_color_list:
-                bag_color_without_bag = bag_color.replace(" bags", "").replace(" bag", "").strip()
-                bag_color_without_number = " ".join(bag_color_without_bag.split(" ")[1:])
-                values.append(bag_color_without_number)
+                bag_color_without_bag = bag_color.replace(" bags", "").replace(" bag", "").strip().split()
+                bag_color_without_number = " ".join(bag_color_without_bag[1:])
+                number = bag_color_without_bag[0]
+                values.append({"color": bag_color_without_number, "amount": int(number)})
 
             rules[key] = values
 
@@ -30,9 +31,14 @@ def parse_rules(list_of_rules: list):
 
 def bag_contains_shiny_gold_bag(bag_color: str, rules: dict):
     shiny_gold = "shiny gold"
+    list_of_colors_for_rule = list()
+    for key in rules[bag_color]:
+        list_of_colors_for_rule.append(key["color"])
+
     if bag_color == shiny_gold:
         return False
-    elif shiny_gold in rules[bag_color]:
+    elif shiny_gold in list_of_colors_for_rule: #list(rules[bag_color]):
+        # enthalten die colors einer bestimmten rule shiny_gold?
         return True
     elif rules[bag_color] == []:
         return False
@@ -40,10 +46,24 @@ def bag_contains_shiny_gold_bag(bag_color: str, rules: dict):
         # Recursive
         sub_bag_rules_list = list(rules[bag_color])
         for sub_bag_rule in sub_bag_rules_list:
-            if bag_contains_shiny_gold_bag(sub_bag_rule, rules=rules):
+            if bag_contains_shiny_gold_bag(sub_bag_rule["color"], rules=rules):
                 return True
 
         return False
+
+
+def sub_amounts(bag_color: str, rules: dict):
+    if rules[bag_color] == []:
+        return 0
+    else:
+        # Recursive
+        sub_bag_rules_list = list(rules[bag_color])
+        sub_amount_for_color = 0
+        for sub_bag_rule in sub_bag_rules_list:
+            sub_amount = sub_bag_rule["amount"] * sub_amounts(sub_bag_rule["color"], rules=rules) + sub_bag_rule["amount"]
+            sub_amount_for_color += sub_amount
+
+        return sub_amount_for_color
 
 
 def first_puzzle(filename: str):
@@ -58,6 +78,16 @@ def first_puzzle(filename: str):
     return bags_containing_gold
 
 
+def second_puzzle(filename: str):
+    raw_rules = read_rules(filename=filename)
+    rules = parse_rules(list_of_rules=raw_rules)
+    return sub_amounts(bag_color="shiny gold", rules=rules)
+
+
 if __name__ == "__main__":
     number_of_rules_containing_gold = first_puzzle(filename="input.txt")
     print(number_of_rules_containing_gold)
+
+    bags_in_gold_bag = second_puzzle(filename="input.txt")
+    print(bags_in_gold_bag)
+
--- 2020/07/day_seven_tests.py
+++ 2020/07/day_seven_tests.py
@@ -5,7 +5,7 @@ from day_seven import *
 class DaySevenPuzzleOneTests(unittest.TestCase):
     def test_bright_white_contains_shiny_gold_bag(self):
         # rule = "bright white bags contain 1 shiny gold bag"
-        rules = {"bright white": ["shiny gold"]}
+        rules = {"bright white": [{"color": "shiny gold", "amount": 1}]}
         bag_color = "bright white"
         expected = True
         result = bag_contains_shiny_gold_bag(bag_color=bag_color, rules=rules)
@@ -13,7 +13,10 @@ class DaySevenPuzzleOneTests(unittest.TestCase):
 
     def test_light_red_contains_shiny_gold_bag(self):
         # rule = "bright white bags contain 1 shiny gold bag"
-        rules = {"bright white": ["shiny gold"], "light red": ["bright white", "muted yellow"]}
+        # rule = "light red bags contain 1 bright white bag, 2 muted yellow bags."
+        rules = {"bright white": [{"color": "shiny gold", "amount": 1}],
+                 "light red": [{"color": "bright white", "amount": 1}, {"color": "muted yellow", "amount": 2}]}
+
         bag_color = "light red"
         expected = True
         result = bag_contains_shiny_gold_bag(bag_color=bag_color, rules=rules)
@@ -28,8 +31,8 @@ class DaySevenPuzzleOneTests(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_gold_shiny(self):
-        # dotted black bags contain no other bags.
-        rules = {"shiny gold": ["dark olive", "vibrant plum"]}
+        # shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        rules = {"shiny gold": [{"color": "dark olive", "amount": 1}, {"color": "vibrant plum", "amount": 2}]}
         bag_color = "shiny gold"
         expected = False
         result = bag_contains_shiny_gold_bag(bag_color=bag_color, rules=rules)
@@ -43,15 +46,40 @@ class DaySevenPuzzleOneTests(unittest.TestCase):
 
     def test_parse_rule_dark_olive(self):
         raw_rule = ["dark olive bags contain 3 faded blue bags, 4 dotted black bags."]
-        expected = {"dark olive": ["faded blue", "dotted black"]}
+        expected = {"dark olive": [{"color": "faded blue", "amount": 3}, {"color": "dotted black", "amount": 4}]}
         result = parse_rules(list_of_rules=raw_rule)
         self.assertEqual(expected, result)
+
+    def test_amount_of_subbags(self):
+        rules = {
+            "shiny gold": [],
+            "bright white": [{"color": "shiny gold", "amount": 1}],
+            "light red": [{"color": "bright white", "amount": 1},
+                          {"color": "muted yellow", "amount": 2}]}
+
+        bag_color = "bright white"
+        expected = 1
+        result = sub_amounts(bag_color=bag_color, rules=rules)
+        self.assertEqual(expected, result)
+
 
     def test_first_puzzle(self):
         expected = 4
         result = first_puzzle(filename="sample_input.txt")
         self.assertEqual(expected, result)
 
+    def test_second_puzzle(self):
+        expected = 32
+        result = second_puzzle(filename="sample_input.txt")
+        self.assertEqual(expected, result)
+
+    def test_vibrant_plum(self):
+        raw_rules = read_rules(filename="sample_input.txt")
+        rules = parse_rules(list_of_rules=raw_rules)
+
+        expected = 11
+        result = sub_amounts(bag_color="vibrant plum", rules=rules)
+        self.assertEqual(expected, result)
 
 if __name__ == '__main__':
     unittest.main()
