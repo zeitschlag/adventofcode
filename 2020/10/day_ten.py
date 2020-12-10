1bbafdaa0687d59cf602154be8687cd6d95b993f
Nathan Mattes <hallo@bullenscheisse.de>
2020-12-10 08:04:14 +0100

    Add day 10, part one
    This was easier than expected :)

---
+++ 2020/10/day_ten.py
@@ -0,0 +1,50 @@
+def read_int_input(filename: str):
+    raw_input = open(filename, "r").read()
+    raw_str_list = raw_input.split("\n")
+    int_list = list()
+    for i in raw_str_list:
+        int_list.append(int(i))
+
+    return int_list
+
+
+def delta(first, second):
+    return max([first, second]) - min([first, second])
+
+
+def highest_joltage_in_list(joltage_list: list):
+    return max(joltage_list)
+
+
+def get_number_of_joltage_differences(joltage_list: list):
+
+    start_voltage = [0]
+    sorted_joltage_list = start_voltage + sorted(joltage_list)
+    deltas = {"1": 0, "3": 0}
+
+    for i in range(0, len(sorted_joltage_list)-1):
+        a = sorted_joltage_list[i]
+        b = sorted_joltage_list[i+1]
+
+        needed_adapter = delta(a, b)
+        if needed_adapter == 1:
+            deltas["1"] += 1
+        elif needed_adapter == 3:
+            deltas["3"] += 1
+
+    # another one three for the built-in adapter
+    deltas["3"] += 1
+
+    return deltas
+
+
+def first_part(filename: str):
+    joltage_list = read_int_input(filename=filename)
+    deltas = get_number_of_joltage_differences(joltage_list=joltage_list)
+    result = deltas["1"] * deltas["3"]
+
+    print("The answer should be {0}".format(result))
+
+
+if __name__ == "__main__":
+    first_part(filename="input.txt")
---
+++ 2020/10/day_ten_tests.py
@@ -0,0 +1,32 @@
+import unittest
+from day_ten import *
+
+class DayTenCase(unittest.TestCase):
+    def test_delta(self):
+        first = 4
+        second = 5
+        expected = second-first
+        result = delta(first=first, second=second)
+        self.assertEqual(expected, result)
+
+    def test_highest_joltage(self):
+        joltage_list = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3]
+        expected = 49
+        result = highest_joltage_in_list(joltage_list=joltage_list)
+        self.assertEqual(expected, result)
+
+    def test_calculate_joltage_differences_first_example(self):
+        joltage_list = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+        expected = {"1": 7, "3": 5}
+        result = get_number_of_joltage_differences(joltage_list=joltage_list)
+        self.assertDictEqual(expected, result)
+
+    def test_calculate_joltage_differences_second(self):
+        joltage_list = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3]
+        expected = {"1": 22, "3": 10}
+        result = get_number_of_joltage_differences(joltage_list=joltage_list)
+        self.assertDictEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
