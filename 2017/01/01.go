f114d1ed7cd45f30a488d272f33390deb8e23704
Nathan Mattes <hallo@bullenscheisse.de>
2021-05-23 12:26:47 +0200

    Clean code

--- 2017/01/01.go
+++ 2017/01/01.go
@@ -3,7 +3,6 @@ package main
 import (
   "fmt"
   "io/ioutil"
-//  "errors"
 )
 
 func readList(filename string) ([]int, error) {
@@ -13,9 +12,6 @@ func readList(filename string) ([]int, error) {
     return nil, err
   }
 
-  // I have some kind of data
-  // I have to make an int-slice out of this data
-
   filecontent := string(data)
 
   var digits []int
@@ -24,7 +20,6 @@ func readList(filename string) ([]int, error) {
     // these seems to be the dirty way to convert a rune to an int
     // https://stackoverflow.com/a/21322694
     digit := int(strDigit - '0')
-
     digits = append(digits, digit)
   }
 
@@ -34,10 +29,8 @@ func readList(filename string) ([]int, error) {
 func calculateCaptcha(digits []int) int {
   var captcha int
   // iterate over all those digits
-  // compare with next one
-  // if they match: add the value to captcha
-  // if you reach the end, compare with first one
   for index, digit := range digits {
+    // if you reach the end, compare with first one
     if index == len(digits)-1 {
       if digit == digits[0] {
         captcha = captcha + digit
@@ -45,7 +38,9 @@ func calculateCaptcha(digits []int) int {
       return captcha
     }
 
+    // compare with next one
     if digit == digits[index+1] {
+      // if they match: add the value to captcha
       captcha = captcha + digit
     }
   }
