693b7aacafdebc7f6ec99fe0cab82c3eeeba968b
Nathan Mattes <hallo@bullenscheisse.de>
2021-12-01 07:52:12 +0100

    Add solution for 01.01
    I'm a bit surprised myself, but if you run AoC2021.Day01.part01 with your input (sorry,
    but you have to insert an Elixir-list by hand for now), it should return the correct
    result, as the test proves!

--- aoc2021/lib/day01/day01.ex
+++ aoc2021/lib/day01/day01.ex
@@ -1,5 +1,10 @@
 defmodule AoC2021.Day01 do
   def count_increases(input) do
-    7
+    Enum.chunk_every(input, 2, 1, :discard)
+    |> Enum.count(fn list -> Enum.sort(list) == list end)
+  end
+
+  def part01(input) do
+    IO.puts(count_increases(input))
   end
 end
