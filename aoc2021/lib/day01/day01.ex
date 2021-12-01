a2f62c0b8e220dc44dd917564993ff56a9832f61
Nathan Mattes <hallo@bullenscheisse.de>
2021-12-01 13:53:44 +0100

    Refactor and cleanup for day 1

--- aoc2021/lib/day01/day01.ex
+++ aoc2021/lib/day01/day01.ex
@@ -1,15 +1,13 @@
 defmodule AoC2021.Day01 do
   def count_increases(input) do
     Enum.chunk_every(input, 2, 1, :discard)
-    |> Enum.count(fn list -> Enum.sort(list) == list end)
+    |> Enum.count(fn [a, b] -> a < b end)
   end
 
   def count_increases_in_windows(input) do
-    sums = Enum.chunk_every(input, 3, 1, :discard)
+    Enum.chunk_every(input, 3, 1, :discard)
     |> Enum.map(&Enum.sum/1)
-
-    Enum.chunk_every(sums, 2, 1, :discard)
-    |> Enum.count(fn [a, b] -> Enum.sort([a, b]) == [a, b] and a != b end)
+    |> count_increases
   end
 
   def part01(input) do
@@ -17,6 +15,6 @@ defmodule AoC2021.Day01 do
   end
 
   def part02(input) do
-    IO.puts count_increases_in_windows(input)
+    IO.puts(count_increases_in_windows(input))
   end
 end
