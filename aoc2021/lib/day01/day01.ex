06585c0e82c070bfeca8f160a7e5a1cce4f72e26
Nathan Mattes <hallo@bullenscheisse.de>
2021-12-01 08:12:22 +0100

    Add possible solution for part 2 of day 1

--- aoc2021/lib/day01/day01.ex
+++ aoc2021/lib/day01/day01.ex
@@ -4,7 +4,19 @@ defmodule AoC2021.Day01 do
     |> Enum.count(fn list -> Enum.sort(list) == list end)
   end
 
+  def count_increases_in_windows(input) do
+    sums = Enum.chunk_every(input, 3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+
+    Enum.chunk_every(sums, 2, 1, :discard)
+    |> Enum.count(fn [a, b] -> Enum.sort([a, b]) == [a, b] and a != b end)
+  end
+
   def part01(input) do
     IO.puts(count_increases(input))
+  end
+
+  def part02(input) do
+    IO.puts count_increases_in_windows(input)
   end
 end
--- aoc2021/test/day01_test.exs
+++ aoc2021/test/day01_test.exs
@@ -1,9 +1,14 @@
 defmodule Day01Tests do
   use ExUnit.Case
-  import AoC2021.Day01, only: [ count_increases: 1 ]
+  import AoC2021.Day01, only: [count_increases: 1, count_increases_in_windows: 1]
 
   test "count 7 encreased" do
-    input = [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ]
+    input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
     assert count_increases(input) == 7
+  end
+
+  test "count 5 encreased using windoes" do
+    input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert count_increases_in_windows(input) == 5
   end
 end
