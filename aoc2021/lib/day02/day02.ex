b280ced4fbbcb9fbd23500a36cc70764e454828c
Nathan Mattes <hallo@bullenscheisse.de>
2021-12-02 20:35:57 +0100

    Add possible solution for day 02 part 02
    with plenty of copy'n'paste and plenty of copy'n'paste-tests

--- aoc2021/lib/day02/day02.ex
+++ aoc2021/lib/day02/day02.ex
@@ -1,56 +1,118 @@
 defmodule AoC2021.Day02 do
+  defmodule Part01 do
+    def calculate_position(depth, h_pos) do
+      depth * h_pos
+    end
 
-  def calculate_position(depth, h_pos) do
-    depth * h_pos
-  end
+    def solve(input) do
+      final_pos =
+        parse(input)
+        |> run
 
-  def part01(input) do
-    final_pos = parse(input)
-    |> run
+      calculate_position(final_pos.depth, final_pos.h_pos)
+    end
 
-    calculate_position(final_pos.depth, final_pos.h_pos)
+    def parse(filename) do
+      # todo: read file, make instructions-map with atoms and int
+      {:ok, content} = File.read(filename)
 
-  end
+      String.split(content, "\n")
+      |> Enum.map(fn str -> String.split(str) end)
+      |> Enum.map(fn [direction, delta] ->
+        %{:direction => String.to_atom(direction), :delta => String.to_integer(delta)}
+      end)
+    end
 
-  def parse(filename) do
-    # todo: read file, make instructions-map with atoms and int
-    {:ok, content} = File.read(filename)
+    def run(instructions) do
+      Enum.reduce(instructions, %{:depth => 0, :h_pos => 0}, fn instr, param ->
+        run(instr, param)
+      end)
+    end
 
-    String.split(content, "\n")
-    |> Enum.map(fn str -> String.split(str) end)
-    |> Enum.map(fn [direction, delta] -> %{ :direction => String.to_atom(direction), :delta => String.to_integer(delta) } end)
-  end
+    def run(instruction, param) do
+      depth = param.depth
+      h_pos = param.h_pos
+      delta = instruction.delta
 
-  def run(instructions) do
-    # follow instructions aka do some magic
-    # returns %{"depth" => 10, "h_pos" => 15}
-    # instructions
-    #|> run(depth, h_pos)
-    Enum.reduce(instructions, %{:depth => 0, :h_pos => 0}, fn instr, param -> run(instr, param)  end)
-  end
+      case instruction.direction do
+        :forward -> forward(delta, depth, h_pos)
+        :up -> up(delta, depth, h_pos)
+        :down -> down(delta, depth, h_pos)
+      end
+    end
 
-  def run(instruction, param) do
-    # instruction = direction and delta
-    # follow instruction and modify either depth or h_pos
-    depth = param.depth
-    h_pos = param.h_pos
-    delta = instruction.delta
-    case instruction.direction do
-      :forward -> forward(delta, depth, h_pos)
-      :up -> up(delta, depth, h_pos)
-      :down -> down(delta, depth, h_pos)
+    def forward(delta, depth, h_pos) do
+      %{:depth => depth, :h_pos => h_pos + delta}
+    end
+
+    def up(delta, depth, h_pos) do
+      %{:depth => depth - delta, :h_pos => h_pos}
+    end
+
+    def down(delta, depth, h_pos) do
+      %{:depth => depth + delta, :h_pos => h_pos}
     end
   end
-
-  def forward(delta, depth, h_pos) do
-    %{ :depth => depth, :h_pos => h_pos + delta }
-  end
   
-  def up(delta, depth, h_pos) do
-    %{ :depth => depth - delta, :h_pos => h_pos }
-  end
+  defmodule Part02 do
+    def calculate_position(depth, h_pos) do
+      depth * h_pos
+    end
 
-  def down(delta, depth, h_pos) do
-    %{ :depth => depth + delta, :h_pos => h_pos }
+    def solve(input) do
+      final_pos =
+        parse(input)
+        |> run
+
+      calculate_position(final_pos.depth, final_pos.h_pos)
+    end
+
+    def parse(filename) do
+      # todo: read file, make instructions-map with atoms and int
+      {:ok, content} = File.read(filename)
+
+      String.split(content, "\n")
+      |> Enum.map(fn str -> String.split(str) end)
+      |> Enum.map(fn [direction, delta] ->
+        %{:direction => String.to_atom(direction), :delta => String.to_integer(delta)}
+      end)
+    end
+
+    def run(instructions) do
+      # follow instructions aka do some magic
+      # returns %{"depth" => 10, "h_pos" => 15}
+      # instructions
+      # |> run(depth, h_pos)
+      Enum.reduce(instructions, %{:depth => 0, :h_pos => 0, :aim => 0}, fn instr, param ->
+        run(instr, param)
+      end)
+    end
+
+    def run(instruction, param) do
+      # instruction = direction and delta
+      # follow instruction and modify either depth or h_pos
+      depth = param.depth
+      h_pos = param.h_pos
+      aim = param.aim
+      delta = instruction.delta
+
+      case instruction.direction do
+        :forward -> forward(delta, depth, h_pos, aim)
+        :up -> up(delta, depth, h_pos, aim)
+        :down -> down(delta, depth, h_pos, aim)
+      end
+    end
+
+    def forward(delta, depth, h_pos, aim) do
+      %{:depth => depth + aim * delta, :h_pos => h_pos + delta, :aim => aim}
+    end
+
+    def up(delta, depth, h_pos, aim) do
+      %{:depth => depth, :h_pos => h_pos, :aim => aim - delta}
+    end
+
+    def down(delta, depth, h_pos, aim) do
+      %{:depth => depth, :h_pos => h_pos, :aim => aim + delta}
+    end
   end
 end
--- aoc2021/test/day02_test.exs
+++ aoc2021/test/day02_test.exs
@@ -1,40 +1,107 @@
 defmodule Day02Test do
-  use ExUnit.Case
-  import AoC2021.Day02, only: [calculate_position: 2, forward: 3, up: 3, down: 3, run: 1, run: 2]
+  defmodule Part01 do
+    use ExUnit.Case
 
-  test "calculate end result with 10 and 15" do
-    horizontal_position = 15
-    depth = 10
-    assert calculate_position(depth, horizontal_position) == 150
+    import AoC2021.Day02.Part01,
+      only: [calculate_position: 2, forward: 3, up: 3, down: 3, run: 1, run: 2]
+
+    test "calculate end result with 10 and 15" do
+      horizontal_position = 15
+      depth = 10
+      assert calculate_position(depth, horizontal_position) == 150
+    end
+
+    test "up instruction with h_pos 0 and depth 10" do
+      h_pos = 0
+      depth = 10
+      assert up(5, depth, h_pos) == %{:depth => depth - 5, :h_pos => h_pos}
+    end
+
+    test "down instruction with h_pos 0 and depth 10" do
+      h_pos = 0
+      depth = 10
+      assert down(5, depth, h_pos) == %{:depth => depth + 5, :h_pos => h_pos}
+    end
+
+    test "forward instruction with h_pos 0 and depth 10" do
+      h_pos = 0
+      depth = 10
+      assert forward(5, depth, h_pos) == %{:depth => depth, :h_pos => 5}
+    end
+
+    test "run example instruction" do
+      instr = %{:direction => :forward, :delta => 20}
+      param = %{:depth => 0, :h_pos => 0}
+
+      assert run(instr, param) == %{:depth => 0, :h_pos => 20}
+    end
+
+    test "follow example instructions" do
+      instructions = [
+        %{:direction => :forward, :delta => 5},
+        %{:direction => :down, :delta => 5},
+        %{:direction => :forward, :delta => 8},
+        %{:direction => :up, :delta => 3},
+        %{:direction => :down, :delta => 8},
+        %{:direction => :forward, :delta => 2}
+      ]
+
+      assert run(instructions) == %{:depth => 10, :h_pos => 15}
+    end
   end
+  
+  defmodule Part02 do
+    use ExUnit.Case
 
-  test "up instruction with h_pos 0 and depth 10" do
-    h_pos = 0
-    depth = 10
-    assert up(5, depth, h_pos) == %{:depth => depth - 5, :h_pos => h_pos}
+    import AoC2021.Day02.Part02,
+      only: [calculate_position: 2, forward: 4, up: 4, down: 4, run: 1, run: 2]
+
+    test "calculate end result with 10 and 15" do
+      horizontal_position = 15
+      depth = 10
+      assert calculate_position(depth, horizontal_position) == 150
+    end
+
+    test "up instruction with h_pos 0 and depth 10" do
+      h_pos = 0
+      depth = 10
+      aim = 0
+      assert up(5, depth, h_pos, aim) == %{:depth => depth, :h_pos => h_pos, :aim => -5}
+    end
+
+    test "down instruction with h_pos 0 and depth 10" do
+      h_pos = 0
+      depth = 10
+      aim = 0
+      assert down(5, depth, h_pos, aim) == %{:depth => depth, :h_pos => h_pos, :aim => 5 }
+    end
+
+    test "forward instruction with h_pos 0 and depth 10" do
+      h_pos = 0
+      depth = 10
+      aim = 0
+      assert forward(5, depth, h_pos, aim) == %{:depth => depth, :h_pos => 5, :aim => 0}
+    end
+
+    test "run example instruction" do
+      instr = %{:direction => :forward, :delta => 20}
+      param = %{:depth => 0, :h_pos => 0, :aim => 0}
+
+      assert run(instr, param) == %{:depth => 0, :h_pos => 20, :aim => 0}
+    end
+
+    test "follow example instructions" do
+      instructions = [
+        %{:direction => :forward, :delta => 5},
+        %{:direction => :down, :delta => 5},
+        %{:direction => :forward, :delta => 8},
+        %{:direction => :up, :delta => 3},
+        %{:direction => :down, :delta => 8},
+        %{:direction => :forward, :delta => 2}
+      ]
+
+      assert run(instructions) == %{:depth => 60, :h_pos => 15, :aim => 10 }
+    end
   end
-
-  test "down instruction with h_pos 0 and depth 10" do
-    h_pos = 0
-    depth = 10
-    assert down(5, depth, h_pos) == %{:depth => depth + 5, :h_pos => h_pos}
-  end
-
-  test "forward instruction with h_pos 0 and depth 10" do
-    h_pos = 0
-    depth = 10
-    assert forward(5, depth, h_pos) == %{:depth => depth, :h_pos => 5}
-  end
-
-  test "run example instruction" do
-    instr = %{ :direction => :forward, :delta => 20 }
-    param = %{ :depth => 0, :h_pos => 0 }
-
-    assert run(instr, param) == %{ :depth => 0, :h_pos => 20  }
-  end
-
-  test "follow example instructions" do
-    instructions = [ %{:direction => :forward, :delta  =>  5}, %{:direction => :down, :delta => 5}, %{:direction => :forward, :delta => 8}, %{:direction => :up, :delta => 3}, %{:direction => :down, :delta => 8}, %{:direction => :forward, :delta => 2} ]
-    assert run(instructions) == %{:depth => 10, :h_pos => 15}
-  end
+  
 end
